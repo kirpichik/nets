901304e910d2ed0a614698b15a0647867d2f6bfd
Kirill Polushin <kirill@polushin.org>
2019-01-13 13:27:21 +0700

    Create SOCKS5 client input parser

--- SOCKS5-Proxy/socks-handler.cpp
+++ SOCKS5-Proxy/socks-handler.cpp
@@ -6,9 +6,10 @@
 
 namespace socks {
 
-static constexpr size_t BUFFER_SIZE = 4096;
 static constexpr std::uint8_t SOCKS5_VERSION = 0x05;
 static constexpr std::uint8_t RESERVED_BYTE = 0x00;
+static constexpr std::uint8_t AUTH_NOT_REQUIRED = 0x00;
+static constexpr std::uint8_t USUPPORTED_AUTH_METHOD = 0xFF;
 
 enum class state : std::uint8_t {
   AUTH_VERSION,
@@ -37,28 +38,147 @@ enum class address_type : std::uint8_t {
 };
 
 enum class response : std::uint8_t {
-  GRANTED,
-  FAILURE,
-  NOT_ALLOWED,
-  NETWORK_UNREACHABLE,
-  HOST_UNREACHABLE,
-  CONNECTION_REFUSED,
-  TTL_EXPIRED,
-  PROTOCOL_ERROR,
-  ADDRESS_TYPE_NOT_SUPPORTED
+  GRANTED = 0x00,
+  FAILURE = 0x01,
+  NOT_ALLOWED = 0x02,
+  NETWORK_UNREACHABLE = 0x03,
+  HOST_UNREACHABLE = 0x04,
+  CONNECTION_REFUSED = 0x05,
+  TTL_EXPIRED = 0x06,
+  PROTOCOL_ERROR = 0x07,
+  ADDRESS_TYPE_NOT_SUPPORTED = 0x08,
+
+  // Спец. для обработчика (не входят в протокол)
+  AUTH_PROTOCOL_ERROR,
+  UNFINISHED,
+  UNSUPPORTED_AUTH_METHOD
 };
 
+socks_handler::socks_handler(std::shared_ptr<proxy::proxy_state> state,
+                             int fd) noexcept
+    : proxy::proxy_handler(state, fd), st(state::AUTH_VERSION) {}
+
+enum response socks_handler::parse_input(const uint8_t* buff,
+                                         size_t len) noexcept {
+  size_t pos = 0;
+  uint8_t value;
+
+  while (pos < len) {
+    switch (st) {
+      case state::AUTH_VERSION:
+        if (buff[pos++] != SOCKS5_VERSION)
+          return response::AUTH_PROTOCOL_ERROR;
+        st = state::AUTH_METHODS_COUNT;
+        break;
+
+      case state::AUTH_METHODS_COUNT:
+        auth_methods_left = buff[pos++];
+        if (auth_methods_left == 0)
+          return response::UNSUPPORTED_AUTH_METHOD;
+        st = state::AUTH_METHODS_LIST;
+        break;
+
+      case state::AUTH_METHODS_LIST:
+        if (buff[pos++] == AUTH_NOT_REQUIRED)
+          auth_method_found = true;
+        if (--auth_methods_left == 0) {
+          if (auth_method_found) {
+            st = state::REQUEST_VERSION;
+            break;
+          }
+          return response::UNSUPPORTED_AUTH_METHOD;
+        }
+        break;
+
+      case state::REQUEST_VERSION:
+        if (buff[pos++] != SOCKS5_VERSION)
+          return response::PROTOCOL_ERROR;
+        st = state::REQUEST_COMMAND;
+        break;
+
+      case state::REQUEST_COMMAND:
+        if (buff[pos++] != static_cast<uint8_t>(command::ESTABLISH_TCP))
+          return response::PROTOCOL_ERROR;
+        st = state::REQUEST_RESERVED_BYTE;
+        break;
+
+      case state::REQUEST_RESERVED_BYTE:
+        if (buff[pos++] != RESERVED_BYTE)
+          return response::PROTOCOL_ERROR;
+        st = state::REQUEST_ADDRESS_TYPE;
+        break;
+
+      case state::REQUEST_ADDRESS_TYPE:
+        value = buff[pos++];
+        if (value == static_cast<uint8_t>(address_type::IPV4)) {
+          addr_type = address_type::IPV4;
+          address.reserve(4);
+        } else if (value == static_cast<uint8_t>(address_type::IPV6)) {
+          addr_type = address_type::IPV6;
+          address.reserve(16);
+        } else
+          addr_type = address_type::HOSTNAME;
+        st = state::REQUEST_ADDRESS;
+        break;
+
+      case state::REQUEST_ADDRESS:
+        value = buff[pos++];
+        address.push_back(value);
+        if ((addr_type == address_type::IPV4 && address.size() == 4) ||
+            (addr_type == address_type::IPV6 && address.size() == 16) ||
+            (addr_type == address_type::HOSTNAME && value == '\0')) {
+          st = state::REQUEST_PORT_FIRST;
+          break;
+        }
+        break;
+
+      case state::REQUEST_PORT_FIRST:
+        port = buff[pos++];
+        st = state::REQUEST_PORT_SECOND;
+        break;
+
+      case state::REQUEST_PORT_SECOND:
+        port = (port << 8) + buff[pos++];
+        return response::GRANTED;
+    }
+  }
+
+  return response::UNFINISHED;
+}
+
+void socks_handler::handle_parse_result(enum response result) noexcept {
+  switch (result) {
+    case response::UNFINISHED:
+      break;
+
+    case response::AUTH_PROTOCOL_ERROR:
+    case response::UNSUPPORTED_AUTH_METHOD:
+      // TODO - send auth error
+      break;
+
+    case response::GRANTED:
+      // TODO - resolve hostname and establish connection
+      break;
+
+    default:
+      // TODO - send protocol error
+      break;
+  }
+}
+
 bool socks_handler::handle_receive() noexcept {
-  char buffer[BUFFER_SIZE];
+  uint8_t buffer[BUFFER_SIZE];
   ssize_t len;
 
   if ((len = recv(fd, buffer, BUFFER_SIZE, 0)) < 0) {
     std::perror("Cannot receive data in socks handle");
     return false;
+  } else if (len == 0) {
+    std::cerr << "Unexpected end-of-stream in socks handler" << std::endl;
+    return false;
   }
 
-  // TODO - switch-case
-
+  handle_parse_result(parse_input(buffer, len));
   return true;
 }
 
--- SOCKS5-Proxy/socks-handler.h
+++ SOCKS5-Proxy/socks-handler.h
@@ -3,27 +3,39 @@
 #define SOCKS_HANDLER_H
 
 #include <vector>
+#include <array>
 
 #include "proxy-state.h"
 
 namespace socks {
 
+static constexpr size_t BUFFER_SIZE = 4096;
+
 enum class state : std::uint8_t;
 enum class address_type : std::uint8_t;
+enum class response : std::uint8_t;
 
 class socks_handler final : public proxy::proxy_handler {
  public:
-  socks_handler(std::shared_ptr<proxy::proxy_state> state, int fd) noexcept
-      : proxy::proxy_handler(state, fd) {}
+  socks_handler(std::shared_ptr<proxy::proxy_state> state, int fd) noexcept;
 
   bool handle_receive() noexcept override;
   bool handle_send() noexcept override;
 
  private:
   enum state st;
+
+  uint8_t auth_methods_left;
+  bool auth_method_found = false;
+
   enum address_type addr_type;
-  std::vector<char> address;
+  std::vector<uint8_t> address;
   uint16_t port;
+
+  std::array<uint8_t, BUFFER_SIZE> output;
+
+  enum response parse_input(const uint8_t* buff, size_t len) noexcept;
+  void handle_parse_result(enum response result) noexcept;
 };
 
 }  // namespace socks
